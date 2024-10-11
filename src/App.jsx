6396446509f824f0aef60cd6f1041d99a90fb6b3
Dhruv Gupta <gdhruv579@gmail.com>
2024-10-12 01:26:30 +0530

    Navbar Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './index.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
   return (
-    <>
-   <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    </>
+   <div>
+    <Navbar/>
+   </div>
   )
 }
 
