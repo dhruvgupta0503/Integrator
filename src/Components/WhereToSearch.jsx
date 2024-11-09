6677253855c64d5c27e58466ebcb8e77575feb30
Anushka-Singh1 <anushka.sng2002@gmail.com>
2024-11-09 15:47:44 +0530

    Added google-map autocomplete search

--- src/Components/WhereToSearch.jsx
+++ src/Components/WhereToSearch.jsx
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  StandaloneSearchBox,
+} from "@react-google-maps/api";
+
+
+const libraries = ["places"];
 
 function WhereToSearch() {
+  const [location, setLocation] = useState("");
+  const navigate = useNavigate();
+  const inputref = useRef(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: import.meta.env.VITE_GOOGLEMAPS_API_KEY,
+    libraries: libraries,
+  });
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/search?location=${location}`);
+  };
+
+  const handleonPlacesChanged = () => {
+    let address = inputref.current.getPlaces();
+    console.log(address);
+  };
+
   return (
-    <div className="flex flex-row mb-12">
-      <div className="bg-[#6D7179] rounded-lg mt-[2%] ml-[5%] mr-[1%] w-5/6 md:w-3/4 lg:w-4/5 pt-3 p-2 flex items-center">
-        <FaSearch className="text-white mr-2" />
-        <input
-          type="text"
-          placeholder="Where To?"
-          className="bg-transparent border-none outline-none text-white w-full"
-        />
-      </div>
+    <div className="my-search-container flex flex-row mb-12 mt-2">
+      {isLoaded && (
+        <div className="bg-[#6D7179] rounded-lg ml-[4%] mt-2 w-[70%] md:w-[70%] lg:w-[66%] p-3 flex items-center">
+          <StandaloneSearchBox
+            onLoad={(ref) => (inputref.current = ref)}
+            onPlacesChanged={handleonPlacesChanged}
+          >
+            <div className="flex items-center w-full">
+              <FaSearch className="text-white mr-2" />
+              <input
+                type="text"
+                placeholder="Where To?"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                className="bg-transparent border-none outline-none text-white w-full"
+              />
+            </div>
+          </StandaloneSearchBox>
+        </div>
+      )}
       <NavLink
+        onClick={handleSearch}
         to="/search"
-        activeClassName=""
-        className="bg-[#6D7179] rounded-lg text-white mt-[2%] text-center mr-[5%] ml-[1%] w-1/6 md:w-1/5 lg:w-1/6 p-2 cursor-pointer">
+        className="bg-[#6D7179] rounded-lg text-white text-center mt-2 mr-[4%] ml-[10%] w-1/6 md:w-1/5 lg:w-1/6 p-2 cursor-pointer"
+      >
         Search
       </NavLink>
     </div>
--- src/pages/SearchRide.jsx
+++ src/pages/SearchRide.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { IoArrowBackCircle } from "react-icons/io5";
-import { NavLink } from "react-router-dom";
-
+import { NavLink, useLocation } from "react-router-dom";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 function SearchRide() {
+  const location = useLocation();
+
+  const params = new URLSearchParams(location.search);
+  const selectedLocation = params.get("location");
+
   return (
-      <>
-      <NavLink to={"/"} >
-      <div className="mx-10 text-[#6D7179] opacity-70 mt-28 hover:text-[#4e5056] cursor-pointer">
-        <IoArrowBackCircle size={50} />
-              </div>
-        </NavLink>
+    <APIProvider
+      apiKey={import.meta.env.VITE_GOOGLEMAPS_API_KEY}
+      onLoad={() => console.log("Maps API has loaded.")}
+    >
+      <NavLink to={"/"}>
+        <div className="mx-10 text-[#6D7179] opacity-70 mt-28 hover:text-[#4e5056] cursor-pointer">
+          <IoArrowBackCircle size={50} />
+        </div>
+      </NavLink>
       <div className="mt-4 mx-10 bg-[#6D7179] p-4 rounded-md opacity-50 min-h-96">
-        Map Integration
+        Map Integration for: {selectedLocation}
       </div>
 
       <div className="relative z-10 flex flex-row">
@@ -26,13 +34,14 @@ function SearchRide() {
           />
         </div>
         <NavLink
-        to="/choose"
-        activeClassName=""
-        className="bg-[#6D7179] rounded-lg text-white mt-[2%] text-center mr-[5%] ml-[1%] w-1/6 md:w-1/5 lg:w-1/6 p-2 cursor-pointer">
+          to="/choose"
+          activeClassName=""
+          className="bg-[#6D7179] rounded-lg text-white mt-[2%] text-center mr-[5%] ml-[1%] w-1/6 md:w-1/5 lg:w-1/6 p-2 cursor-pointer"
+        >
           Search
         </NavLink>
       </div>
-    </>
+    </APIProvider>
   );
 }
 
