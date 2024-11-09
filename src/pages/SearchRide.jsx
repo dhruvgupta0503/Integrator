ce766ae5e2c9d5ed73cb3000c5b25dd39e91647c
Anushka-Singh1 <anushka.sng2002@gmail.com>
2024-11-09 17:53:07 +0530

    Updated SearchRide

--- src/pages/SearchRide.jsx
+++ src/pages/SearchRide.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
 import { IoArrowBackCircle } from "react-icons/io5";
 import { NavLink, useLocation } from "react-router-dom";
 import { APIProvider } from "@vis.gl/react-google-maps";
@@ -25,20 +24,12 @@ function SearchRide() {
       </div>
 
       <div className="relative z-10 flex flex-row">
-        <div className="bg-[#6D7179] rounded-lg mt-[2%] ml-[5%] mr-[1%] w-5/6 md:w-3/4 lg:w-4/5 pt-3 p-2 flex items-center">
-          <FaSearch className="text-white mr-2" />
-          <input
-            type="text"
-            placeholder="Search your ride..."
-            className="bg-transparent border-none outline-none text-white w-full"
-          />
-        </div>
         <NavLink
           to="/choose"
           activeClassName=""
-          className="bg-[#6D7179] rounded-lg text-white mt-[2%] text-center mr-[5%] ml-[1%] w-1/6 md:w-1/5 lg:w-1/6 p-2 cursor-pointer"
+          className="bg-[#6D7179] rounded-lg text-white mt-[3%] mx-auto text-center justify-self-center w-3/6 md:w-2/6 lg:w-1/6 p-2 cursor-pointer"
         >
-          Search
+          Search Your Ride
         </NavLink>
       </div>
     </APIProvider>
